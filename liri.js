6a32c55fb068357542c58c2944dfc3602c59e8c3
Khamhuang, David <khameye19888@gmail.com>
2019-07-06 20:20:57 -0500

    API calls with movie,concert-this

--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
-//var dot = require("dotenv").config();
-
-// importing files from 'keys.js'
-console.log("Both this and what was exported from the keys file should print.");
-var keys = require("./keys.js");
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
 //var spotify = new Spotify(keys.spotify);
 
+
+// importing files from 'keys.js'
+var keys = require("./keys.js");
 var request = require('request');
 var axios = require('axios');
 var moment = require('moment');
+
 
 
 var arg1 = process.argv[2];
@@ -21,42 +22,45 @@ var startProg = function (arg1, arg2) {
     switch (arg1) {
         case "concert-this": getMyBands(arg2);
             break;
-        case "movie-this": getMovieInfo(arg2);
+            case "movie-this": getMovieInfo(arg2);
             break;
-        case "spotify-this-song": getSongInfo(arg2);
+            case "spotify-this-song": getSongInfo(arg2);
             break;
-        case "do-what-it-says": getWhat(arg2);
+            case "do-what-it-says": getWhat(arg2);
             break;
-        default:
+            default:
             console.log("enter the right action please")
     }
 }
 
 function getMovieInfo(movieName) {
+    var divider = "\n------------------------------------------------------------";
     console.log("get movie");
     if (movieName === undefined) {
         movieName = "Mr Nobody";
     }
 
     var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
-
+    
     axios.get(movieUrl)
-        .then(function (response) {
-           console.log(response);
+    .then(function (response) {
+        console.log(response);
             var jsonData = response.data;
 
             var movieData = [
+                divider,
                 "Title: " + jsonData.Title,
                 "Year: " + jsonData.Year,
                 "IMDB: " + jsonData.imdbRating,
-               "Rotten Tomatoes Rating: " + jsonData.Ratings.tomatoRating,
+                "Rotten Tomatoes Rating: " + jsonData.Ratings.tomatoRating,
                 "Country: " + jsonData.Country,
                 "Plot: " + jsonData.Plot,
-                "Actors: " + jsonData.Actors
+                "Actors: " + jsonData.Actors,
+                divider
             ].join("\n\n");
-
+            
             console.log(movieData);
-
+            
         })
         .catch(function (error) {
             console.log(error);
@@ -65,10 +69,10 @@ function getMovieInfo(movieName) {
 
         });
     }
-
+    
     function getMyBands(artist) {
         console.log ("get tickets");
-
+        var divider = "\n------------------------------------------------------------";
         
         var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
         axios.get(concertURL)
@@ -76,25 +80,21 @@ function getMovieInfo(movieName) {
             console.log(response);
             var jsonData = response.data[0];
             var concertTime = jsonData.datetime;
+            
+            var concertData = [
+                divider,
+                "Venue: " + jsonData.venue.name,
+                "Location of Venue: " + jsonData.venue.city,
+                "Time of Event: " + moment(concertTime).format('MM/DD/YYYY h:mm:ss a'),
+                divider
+            ].join("\n\n");
+            console.log(concertData);
+            
 
-                var concertData = [
-                    "Venue: " + jsonData.venue.name,
-                    "Location of Venue: " + jsonData.venue.city,
-                    "Time of Event: " + moment(concertTime).format('MM/DD/YYYY h:mm:ss a')
-                    
-                ].join("\n\n");
-                console.log(concertData);
-                
-
-            })
-
+        })
+        
     }
-
-    function getSongInfo(song) {
-        console.log("get song INFO");
-        if (song === undefined) {
-            song = "The Sign";
-        }
-    }
+    
+   
 
     startProg(arg1, arg2);
